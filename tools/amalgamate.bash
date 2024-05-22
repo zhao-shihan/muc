d4fa4ebec27c3f0319aa79e18ed24d686b418205
zhaoshihan <zhao_shihan@foxmail.com>
2024-05-23 02:36:12 +0800

    Update amalgamation

--- tools/amalgamate.bash
+++ tools/amalgamate.bash
@@ -3,6 +3,6 @@
 if test -f muc-universe.template; then
     mkdir build
     cp muc-universe.template build/muc-universe.h++
-    amalgamate/amalgamate -o build -I include -- build/muc-universe.h++
+    amalgamate/amalgamate -v -o build -I include -- build/muc-universe.h++
     cp build/muc-amalgamated/muc.h++ include/muc-universe
 fi
