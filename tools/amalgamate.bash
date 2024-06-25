67cbc2376026d87e3293cbacc7d0cfd97b306f26
zhaoshihan <zhao_shihan@foxmail.com>
2024-06-25 19:23:18 +0800

    Update file header

--- tools/amalgamate.bash
+++ tools/amalgamate.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if test -f muc-universe.template; then
+if test -f muc-universe.in; then
     mkdir build
-    cp muc-universe.template build/muc-universe.h++
+    cp muc-universe.in build/muc-universe.h++
     amalgamate/amalgamate -v -o build -I include -- build/muc-universe.h++
     cp build/muc-amalgamated/muc.h++ include/muc-universe
 else
